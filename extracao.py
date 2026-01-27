de3772b1ea9da31ea64d8d03667efe3bc79cdede
Paulo Martins <pmartins@hey.com>
2026-01-27 12:23:16 -0300

    extracao salva raw e processed

--- extracao.py
+++ extracao.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import pandas as pd
+import json
 
 def main():
     response = requests.get("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2")
@@ -8,10 +9,14 @@ def main():
 
     if response.ok:
         data = response.json()
+
+        with open(f"data/raw/{date_today}_facts.json"):
+            json.dump(data, f, indent=2, ensure_ascii = False)
+
         df = pd.DataFrame(data)
         df_filtrada = df[["_id", "text", "updatedAt", "sentCount"]]
         df_filtrada.columns = ["id", "text", "updated", "sent_count"]
-        df_filtrada.to_csv(f"data/{date_today}_facts.csv", index = False)
+        df_filtrada.to_csv(f"data/processed/{date_today}_facts.csv", index = False)
         
     else:
         print(f"Error: Status code - {response.status_code}")
