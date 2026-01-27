461bafc2e8b5521912e9f1c6f02782a3a1c22092
Paulo Martins <pmartins@hey.com>
2026-01-27 10:05:27 -0300

    adicionada codigo para extracao

---
+++ extracao.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+import pandas as pd
+
+def main():
+    response = requests.get("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2")
+    date_today = datetime.today().strftime("%Y-%m-%d")
+
+    if response.ok:
+        data = response.json()
+        df = pd.DataFrame(data)
+        df_filtrada = df[["_id", "text", "updatedAt", "sentCount"]]
+        df_filtrada.columns = ["id", "text", "updated", "sent_count"]
+        df_filtrada.to_csv(f"data/{date_today}_facts.csv", index = False)
+        
+    else:
+        print(f"Error: Status code - {response.status_code}")
+
+if __name__ == "__main__":
+    main()
